81cc1be07afdf7d045d8329ef76e758440f1ec92
Catherine A Bernaciak <cbernaci@hh-1305-12.andrew.local.cmu.edu>
2023-04-11 19:06:11 -0400

    adding AES-Core-enging--master but it doesnt work

--- AES-Core-engine--master/output_files/AES_tb.v
+++ AES-Core-engine--master/output_files/AES_tb.v
@@ -1,11 +1,9 @@
-module AES_main(
-    input CLOCK_50,
-    output wire [9:0] LEDR);
-
-
+module AES_tb();
+    
 wire [127:0] data_out;
 wire [127:0] data_in;
 wire [127:0] key;
+wire [9:0] LEDR;
 
 assign data_in = 128'h_00112233445566778899aabbccddeeff;
 assign key = 128'h_000102030405060708090a0b0c0d0e0f;
@@ -16,7 +14,12 @@ aes(.clk(CLOCK_50),
     .data_out(data_out));
 
 assign LEDR[0] = (data_out == 128'h_69c4e0d86a7b0430d8cdb78070b4c55a) ? 1'b1 : 1'b0;
-assign LEDR[1] = 1'b1;
+
+initial begin
+	$monitor("key = %h, data_in = %h, data_out = %h, LEDR0 = %b", key, data_in, data_out, LEDR[0]);
+
+end
+
 
 endmodule
 
