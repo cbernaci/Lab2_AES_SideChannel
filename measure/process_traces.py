1b3165ee0057b77bf635e7b368fd95e21701882b
Jerry Wu <jerrywuzijie@gmail.com>
2023-04-22 12:33:36 -0400

    feat: get .npy files in data/ folder

--- measure/process_traces.py
+++ measure/process_traces.py
@@ -1,17 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+# Define the path to the directory containing the algorithm subfolders
+path = 'data'
+
+# Loop over each subdirectory in the algorithm directory
+for dirname in os.listdir(path):
+    fullpath = os.path.join(path, dirname)
+    print(dirname.rstrip('.npy'))
 
 
-fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+# fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
 
 
-voltages = np.load('partial_samples.npy')
-ax1.plot(voltages)
+voltages = np.load("data/00112233445566778899AABBCCDDEEFF.npy")
+print(voltages.shape)
+sample = -1
+plt.plot(voltages[0][:sample])
+print(voltages[0][:sample].mean())
+# plt.plot(voltages[1][:sample])
 
-ax2.plot(np.load("samples.npy"))
-
-# window_size = 100
-# ma = np.convolve(voltages, np.ones(window_size)/window_size, mode='valid')
-# plt.plot(ma)
+window_size = 1000
+ma = np.convolve(voltages[0][:sample], np.ones(window_size)/window_size, mode='valid')
+plt.plot(ma)
 
 plt.show()
