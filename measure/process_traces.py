10213c6023250a7670eb120178f7fc091789a5f6
Jerry Wu <jerrywuzijie@gmail.com>
2023-04-23 20:34:27 -0400

    feat: make it a function return to calculated power traces as an np 2d array

--- measure/process_traces.py
+++ measure/process_traces.py
@@ -1,31 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
-# Define the path to the directory containing the algorithm subfolders
-path = 'data'
-
-# Loop over each subdirectory in the algorithm directory
-for dirname in os.listdir(path):
-    if dirname.endswith('.npy'):
-        plain_text = dirname[:-4]
-        plt.title(f"trace of {plain_text}")
-
-        full_path = os.path.join(path, dirname)
-        voltages = np.load(full_path)
-        sample = -1
-        plt.plot(voltages[0][:sample], label='trace')
-        plt.plot(voltages[1][:sample], label='trigger')
-
-        window_size = 1000
-        ma = np.convolve(voltages[0][:sample], np.ones(
-            window_size)/window_size, mode='valid')
-        plt.plot(ma, label='moving average')
-
-        plt.legend()
-        plt.show()
-    
-    break
+import time
 
 
-# fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+def get_power_trace(num_of_traces, path, VCC):
+    """
+    get the power traces as np array
+
+    return: plaintext_lst, power_traces
+    """
+    # Check if the path is valid
+    if not os.path.isdir(path):
+        raise NotADirectoryError('Invalid path: {}'.format(path))
+
+    power_traces = []
+    plaintext_lst = []
+
+    # Loop over the directory and process the first num_of_traces traces
+    for dirname in os.listdir(path)[:num_of_traces]:
+        if dirname.endswith('.npy'):
+            plain_text = dirname[:-4]
+            plaintext_lst.append(int(plain_text, 16))
+
+            full_path = os.path.join(path, dirname)
+            trace, trigger = np.load(full_path)
+            start_i = 0
+            for i in range(len(trigger)):
+                if trigger[i] > 1:
+                    start_i = i
+                    break
+            trace = trace[start_i:]
+            if power_traces:
+                # if power_traces is not empty, make later traces the same size as the first one
+                if len(trace) < len(power_traces[0]):
+                    # if shorter, pad with 0s
+                    trace = np.pad(
+                        trace, (0, len(power_traces[0]) - len(trace)), 'constant')
+                elif len(trace) > len(power_traces[0]):
+                    # if longer, truncate
+                    trace = trace[:len(power_traces[0])]
+            # note: the resistance value doesn't matter, since
+            # P = IV = (VCC-V) * (V/R) is proportional to (VCC-V) * V
+            power_traces.append((VCC - trace) * trace)
+
+    return np.array(plaintext_lst), np.array(power_traces)
